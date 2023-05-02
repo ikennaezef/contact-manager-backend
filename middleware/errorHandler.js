00b3c829fa5b6da692190c51450ac0cc4d9fc90e
Ikenna <iykee0333@gmail.com>
2023-05-02 13:25:31 +0100

    tokenHandler middleware

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -5,35 +5,35 @@ const errorHandler = (err, req, res, next) => {
 		case constants.VALIDATION_ERROR:
 			res.json({
 				title: "Validation Failed",
-				mesasge: err.message,
+				message: err.message,
 				stackTrace: err.stack,
 			});
 			break;
 		case constants.FORBIDDEN:
 			res.json({
 				title: "Forbidden",
-				mesasge: err.message,
+				message: err.message,
 				stackTrace: err.stack,
 			});
 			break;
 		case constants.UNAUTHORIZED:
 			res.json({
 				title: "Unauthorized",
-				mesasge: err.message,
+				message: err.message,
 				stackTrace: err.stack,
 			});
 			break;
 		case constants.NOT_FOUND:
 			res.json({
 				title: "Not Found",
-				mesasge: err.message,
+				message: err.message,
 				stackTrace: err.stack,
 			});
 			break;
 		case constants.SERVER_ERROR:
 			res.json({
 				title: "Server Error",
-				mesasge: err.message,
+				message: err.message,
 				stackTrace: err.stack,
 			});
 			break;
