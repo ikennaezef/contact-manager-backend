53a45f249c5aab8f22d60a1e7feb4ffcbd7078fb
Ikenna <iykee0333@gmail.com>
2023-05-02 13:52:11 +0100

    verify token middleware

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -5,6 +5,11 @@ const validateToken = asyncHandler(async (req, res, next) => {
 	let token;
 	let authHeader = req.headers.authorization || req.headers.Authorization;
 
+	if (!authHeader) {
+		res.status(401);
+		throw new Error("Authorization token missing");
+	}
+
 	if (authHeader && authHeader.startsWith("Bearer")) {
 		token = authHeader.split(" ")[1];
 		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
